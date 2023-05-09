8aa7429b18dcae609ca3e71d386e5da1bb561b7a
Shourov Foisal <shourovfoisal@gmail.com>
2023-05-09 15:29:47 +0600

    Attempt failed

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # r2dbc-without-spring-data-r2dbc
  Testing r2dbc without using spring data r2dbc
+
+This attempt failed
--- src/main/java/com/example/r2dbcwithoutspringdatar2dbc/R2dbcWithoutSpringDataR2dbcApplication.java
+++ src/main/java/com/example/r2dbcwithoutspringdatar2dbc/R2dbcWithoutSpringDataR2dbcApplication.java
@@ -3,10 +3,23 @@ package com.example.r2dbcwithoutspringdatar2dbc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class R2dbcWithoutSpringDataR2dbcApplication {
 
 	public static void main(String[] args) {
+
+		try {
+//			Class.forName("org.postgresql.Driver");
+			Connection connection = DriverManager.getConnection("r2dbc:postgresql://localhost:5432/reactiveraw", "postgres", "newpassword");
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		}
+
 		SpringApplication.run(R2dbcWithoutSpringDataR2dbcApplication.class, args);
 	}
 
